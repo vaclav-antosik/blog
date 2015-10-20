adc5e0b8ca93f98b98627a68846f32f73b3936e7
vaclav-antosik <vaclavantosik@hotmail.com>
2015-10-20 15:45:01 +0200

    ssh deployment 1

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,42 @@
-var ftp = require('vinyl-ftp');
+var SSH = require('simple-ssh');
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 var minimist = require('minimist');
 var args = minimist(process.argv.slice(2));
 
-gulp.task('deploy', function() {
-  var remotePath = '/kpjs_deploy/';
-  var conn = ftp.create({
-    host: args.address,
-    user: args.user,
-    password: args.password, /*travis encrypt FTP_PASSWORD=*** --add*/
-    log: gutil.log
-  });
- 
-  gulp.src(['./**','!./node_modules','!./node_modules/**'])
-    .pipe(conn.newer(remotePath))
-    .pipe(conn.dest(remotePath));
+var ssh = new SSH({
+    host: '104.41.201.75',
+    user: 'ftpuser',
+    pass: 'kpjs.123',
+    baseDir:'c:\\!\\www\\blog'
+});
+
+gulp.task('deploy', function(done) {
+
+ssh.exec('git pull origin master', {
+    out: function(stdout) {
+        console.log(stdout);
+    },
+    err: function(stderr) {
+        console.log(stderr); // this-does-not-exist: command not found
+    },
+	exit: function(code) {
+        console.log("EXIT: " + code);
+    }
+})
+.exec('npm install', {
+    out: function(stdout) {
+        console.log(stdout);
+		done(null);
+		console.log("DONE");
+    },
+    err: function(stderr) {
+        console.log(stderr); // this-does-not-exist: command not found
+    },
+	exit: function(code) {
+        console.log("EXIT: " + code);
+    }
+})
+.start();
+
 });
